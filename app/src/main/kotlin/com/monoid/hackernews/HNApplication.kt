84a65d52ff38eb05a705f1362f06befc8140e4b6
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2022-05-03 11:13:52 -0400

    fix: Fixed coroutine scope for upvotes and favorites update job.

--- app/src/main/kotlin/com/monoid/hackernews/HNApplication.kt
+++ app/src/main/kotlin/com/monoid/hackernews/HNApplication.kt
@@ -14,7 +14,6 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.launch
-import kotlinx.datetime.Clock
 import java.util.concurrent.CancellationException
 import java.util.concurrent.TimeUnit
 
@@ -51,47 +50,44 @@ class HNApplication : Application() {
         val favoriteDao = db.favoriteDao()
 
         // Update upvote and favorite table on login and then periodically.
-        CoroutineScope(Dispatchers.Main.immediate).launch {
+        coroutineScope.launch {
             settingsDataStore.data.distinctUntilChanged().collectLatest { authentication ->
                 if (authentication.password?.isNotEmpty() == true) {
                     while (true) {
                         try {
-                            val upvoteApi =
+                            val upvoteDef =
                                 async {
-                                    getUpvoted(
-                                        authentication,
-                                        Username(authentication.username)
+                                    upvoteDao.replaceUpvotesForUser(
+                                        username = authentication.username,
+                                        upvotes = getUpvoted(
+                                            authentication,
+                                            Username(authentication.username)
+                                        ),
                                     )
                                 }
 
-                            val favoriteApi =
-                                async { getFavorites(Username(authentication.username)) }
+                            val favoriteDef =
+                                async {
+                                    favoriteDao.replaceFavoritesForUser(
+                                        username = authentication.username,
+                                        favorites = getFavorites(Username(authentication.username)),
+                                    )
+                                }
 
-                            val upvoteDb = async {
-                                upvoteDao.replaceUpvotesForUser(
-                                    username = authentication.username,
-                                    upvotes = upvoteApi.await(),
-                                )
-                            }
+                            upvoteDef.await()
+                            favoriteDef.await()
 
-                            val favoriteDb = async {
-                                favoriteDao.replaceFavoritesForUser(
-                                    username = authentication.username,
-                                    favorites = favoriteApi.await()
-                                )
-                            }
-
-                            upvoteDb.await()
-                            favoriteDb.await()
-
-                            delay(
-                                TimeUnit.HOURS.toMillis(
-                                    resources.getInteger(R.integer.favorites_state_hours).toLong()
-                                ) - Clock.System.now().toEpochMilliseconds()
-                            )
+                            throw Exception()
                         } catch (error: Throwable) {
                             if (error is CancellationException) throw error
+                            error.printStackTrace()
                         }
+
+                        delay(
+                            TimeUnit.HOURS.toMillis(
+                                resources.getInteger(R.integer.favorites_state_hours).toLong()
+                            )
+                        )
                     }
                 }
             }
