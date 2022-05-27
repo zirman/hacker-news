75ea77c6325945888b44ba49dda9de27912b178e
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2022-05-27 11:17:05 -0400

    deps: Updated AGP to 7.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     }
     dependencies {
         // must import plugins in top level or kotlin compiler gets ir errors
-        classpath("com.android.tools.build:gradle:7.2.0")
+        classpath("com.android.tools.build:gradle:7.2.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
         classpath("org.jetbrains.kotlin:kotlin-serialization:1.6.21")
         classpath("com.google.protobuf:protobuf-gradle-plugin:0.8.18")
