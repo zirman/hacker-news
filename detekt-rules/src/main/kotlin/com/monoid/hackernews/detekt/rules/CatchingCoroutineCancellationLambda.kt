0391c8f13177a118fb30312ccb17e07a0cbf577d
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2024-07-14 12:28:24 -0400

    Added CatchingCoroutineCancellationLambda Detekt rule that uses type resolution for robustly checking for handling catching of coroutine cancellation

--- detekt-rules/src/main/kotlin/com/monoid/hackernews/detekt/rules/CatchingCoroutineCancellationLambda.kt
+++ detekt-rules/src/main/kotlin/com/monoid/hackernews/detekt/rules/CatchingCoroutineCancellationLambda.kt
@@ -13,6 +13,7 @@ import org.jetbrains.kotlin.com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.descriptors.FunctionDescriptor
 import org.jetbrains.kotlin.psi.KtCallExpression
 import org.jetbrains.kotlin.psi.KtCallableReferenceExpression
+import org.jetbrains.kotlin.psi.KtCatchClause
 import org.jetbrains.kotlin.psi.KtExpression
 import org.jetbrains.kotlin.psi.KtLambdaExpression
 import org.jetbrains.kotlin.psi.KtNamedFunction
@@ -28,135 +29,116 @@ import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameOrNull
 
 @RequiresTypeResolution
 class CatchingCoroutineCancellationLambda(config: Config = Config.empty) : Rule(config) {
+
     override val issue = Issue(
-        javaClass.simpleName,
-        Severity.Defect,
-        "This rule reports not handling coroutine cancellation exceptions correctly.",
-        Debt.FIVE_MINS
+        id = javaClass.simpleName,
+        severity = Severity.Defect,
+        description = "This rule reports not handling coroutine cancellation exceptions correctly.",
+        debt = Debt.FIVE_MINS,
     )
 
-    override fun visitCallExpression(expression: KtCallExpression) {
-        super.visitCallExpression(expression)
-        checkAndReport(expression)
+    override fun visitCatchSection(catchClause: KtCatchClause) {
+        super.visitCatchSection(catchClause)
+        checkAndReport(catchClause)
     }
 
-    override fun visitCallableReferenceExpression(expression: KtCallableReferenceExpression) {
-        super.visitCallableReferenceExpression(expression)
-        checkAndReport(expression)
-    }
-
-    private fun checkAndReport(expression: KtExpression) {
-        if (expression.isThreadSleepFunction() && shouldReport(expression)) {
-            report(CodeSmell(issue, Entity.from(expression), SUSPEND_FUN_MESSAGE))
-        }
-    }
-
-    private fun KtExpression.isThreadSleepFunction(): Boolean {
-        fun KtCallableReferenceExpression.isSleepCallableRef(): Boolean {
-            return if (this.parent is KtValueArgument) {
-                // Only checking if this is used as for invocation
-                this.callableReference.isThreadSleepFunction()
-            } else {
-                false
-            }
-        }
-        return if (this is KtCallableReferenceExpression) {
-            this.isSleepCallableRef()
-        } else {
-            getResolvedCall(bindingContext)
-                ?.resultingDescriptor
-                ?.fqNameOrNull()
-                ?.asString() == FQ_NAME
+    private fun checkAndReport(catchClause: KtCatchClause) {
+        if (shouldReport(catchClause)) {
+            report(
+                CodeSmell(
+                    issue = issue,
+                    entity = Entity.from(catchClause),
+                    message = "Check for coroutine cancellation with `currentCoroutineContext().ensureActive()`",
+                )
+            )
         }
     }
 
-    @Suppress("ReturnCount")
-    private fun getNearestParentForSuspension(psiElement: PsiElement): PsiElement? {
-        fun KtValueArgument.isNearestParentForSuspension(): Boolean {
-            val parent = this.getParentOfTypes(true, KtCallExpression::class.java) ?: return false
-            val callDescriptor = parent.getResolvedCall(bindingContext)
-            val valueParameterDescriptor =
-                parent.getResolvedCall(bindingContext)
-                    ?.getParameterForArgument(this)
-                    ?: return false
-            val functionDescriptor = callDescriptor?.resultingDescriptor as? FunctionDescriptor ?: return false
-            return functionDescriptor.isInline.not() ||
-                (valueParameterDescriptor.isNoinline || valueParameterDescriptor.isCrossinline)
-        }
-        return psiElement.getParentOfTypesAndPredicate(
-            false,
-            KtNamedFunction::class.java,
-            KtValueArgument::class.java,
-            KtLambdaExpression::class.java,
-        ) {
-            when (it) {
-                is KtValueArgument -> {
-                    it.isNearestParentForSuspension()
-                }
-
-                is KtNamedFunction -> {
-                    true
-                }
-
-                is KtLambdaExpression -> {
-                    it.getParentOfType<KtProperty>(true, KtValueArgument::class.java) != null
-                }
-
-                else -> {
-                    false
-                }
-            }
-        }
-    }
-
-    private fun PsiElement.isSuspendAllowed(): Boolean {
-        return when (this) {
-            is KtValueArgument -> {
-                this.isSuspendAllowed()
-            }
-
-            is KtNamedFunction -> {
-                this.isSuspendAllowed()
-            }
-
-            is KtLambdaExpression -> {
-                this.isSuspendAllowed()
-            }
-
-            else -> {
-                false
-            }
-        }
+    private fun PsiElement.isSuspendAllowed(): Boolean = when (this) {
+        is KtValueArgument -> isSuspendAllowed()
+        is KtNamedFunction -> isSuspendAllowed()
+        is KtLambdaExpression -> isSuspendAllowed()
+        else -> false
     }
 
     private fun KtValueArgument.isSuspendAllowed(): Boolean {
-        val parent = this.getParentOfTypes(true, KtCallExpression::class.java) ?: return false
+        val parent = getParentOfTypes(strict = true, KtCallExpression::class.java) ?: return false
         val valueParameterDescriptor =
-            parent.getResolvedCall(bindingContext)
-                ?.getParameterForArgument(this)
+            parent.getResolvedCall(bindingContext)?.getParameterForArgument(this)
         return valueParameterDescriptor?.returnType?.isSuspendFunctionType == true
     }
 
     private fun KtLambdaExpression.isSuspendAllowed(): Boolean {
-        val parent = this.getParentOfTypes(true, KtProperty::class.java)
+        val parent = getParentOfTypes(strict = true, KtProperty::class.java) ?: return false
+        return bindingContext[BindingContext.VARIABLE, parent]?.returnType?.isSuspendFunctionType == true
+    }
+
+    private fun KtNamedFunction.isSuspendAllowed(): Boolean =
+        bindingContext[BindingContext.FUNCTION, this]?.isSuspend == true
+
+    private fun shouldReport(catchClause: KtCatchClause): Boolean =
+        catchClause.catchesCoroutineCancellation() &&
+            catchClause.nearestParent()?.isSuspendAllowed() == true &&
+            catchClause.isEnsureActiveCalled().not()
+
+    private fun KtCatchClause.catchesCoroutineCancellation(): Boolean =
+        catchParameter?.typeReference?.let { bindingContext[BindingContext.TYPE, it] }
+            ?.constructor?.declarationDescriptor?.fqNameOrNull()
+            ?.asString()
+            ?.let { possibleCoroutineCancellationFqNames.contains(it) } == true
+
+    private fun KtCatchClause.nearestParent(): PsiElement? = getParentOfTypesAndPredicate(
+        strict = false,
+        KtNamedFunction::class.java,
+        KtValueArgument::class.java,
+        KtLambdaExpression::class.java,
+    ) {
+        when (it) {
+            is KtValueArgument -> it.isNearestParentForSuspension()
+            is KtNamedFunction -> true
+            is KtLambdaExpression ->
+                it.getParentOfType<KtProperty>(strict = true, KtValueArgument::class.java) !=
+                    null
+
+            else -> false
+        }
+    }
+
+    private fun KtCatchClause.isEnsureActiveCalled(): Boolean =
+        catchBody?.children?.firstOrNull()
+            .let { it as? KtExpression }
+            ?.isEnsureActiveExpression() == true
+
+    @Suppress("ReturnCount")
+    private fun KtValueArgument.isNearestParentForSuspension(): Boolean {
+        val parent = getParentOfTypes(true, KtCallExpression::class.java) ?: return false
+        val valueParameterDescriptor = parent.getResolvedCall(bindingContext)
+            ?.getParameterForArgument(this)
             ?: return false
-        val properDescriptor = bindingContext[BindingContext.VARIABLE, parent] ?: return false
-        return properDescriptor.returnType?.isSuspendFunctionType ?: false
+        val functionDescriptor = parent.getResolvedCall(bindingContext)
+            ?.resultingDescriptor
+            ?.let { it as? FunctionDescriptor }
+            ?: return false
+        return functionDescriptor.isInline.not() ||
+            (valueParameterDescriptor.isNoinline || valueParameterDescriptor.isCrossinline)
     }
 
-    private fun KtNamedFunction.isSuspendAllowed(): Boolean {
-        val functionDescriptor = bindingContext[BindingContext.FUNCTION, this] ?: return false
-        return functionDescriptor.isSuspend
-    }
+    private fun KtExpression.isEnsureActiveExpression(): Boolean = when {
+        this !is KtCallableReferenceExpression -> getResolvedCall(bindingContext)
+            ?.resultingDescriptor
+            ?.fqNameOrNull()
+            ?.asString() == "kotlinx.coroutines.ensureActive"
 
-    private fun shouldReport(expression: KtExpression): Boolean {
-        val nearestParentForSuspension = getNearestParentForSuspension(expression) ?: return false
-        return nearestParentForSuspension.isSuspendAllowed()
+        parent is KtValueArgument -> callableReference.isEnsureActiveExpression()
+        else -> false
     }
 
     companion object {
-        private const val SUSPEND_FUN_MESSAGE =
-            "This use of Thread.sleep() inside a suspend function should be replaced by delay()."
-        private const val FQ_NAME = "java.lang.Thread.sleep"
+        private val possibleCoroutineCancellationFqNames = listOf(
+            "kotlin.Throwable",
+            "java.lang.Exception",
+            "java.lang.Throwable",
+            "java.util.concurrent.CancellationException",
+        )
     }
 }
