2c4e08167bf28998e48be8930310822d4362c268
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2024-10-06 10:44:09 -0400

    Moved html string files into html package

--- common/view/src/commonMain/kotlin/com/monoid/hackernews/common/view/aboutus/AboutUs.kt
+++ common/view/src/commonMain/kotlin/com/monoid/hackernews/common/view/aboutus/AboutUs.kt
@@ -22,7 +22,7 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.monoid.hackernews.common.view.Res
 import com.monoid.hackernews.common.view.about_us_detail_html
-import com.monoid.hackernews.common.view.rememberAnnotatedHtmlString
+import com.monoid.hackernews.common.view.html.rememberAnnotatedHtmlString
 import org.jetbrains.compose.resources.stringResource
 
 @Composable
