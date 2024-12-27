add84a3a1cddf5043e4406859310cd5732fa1d4a
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2024-12-26 20:36:50 -0500

    iOS loads home screen

--- common/data/src/iosMain/kotlin/com/monoid/hackernews/common/data/URL.ios.kt
+++ common/data/src/iosMain/kotlin/com/monoid/hackernews/common/data/URL.ios.kt
@@ -1,7 +1,10 @@
 package com.monoid.hackernews.common.data
 
+import platform.Foundation.NSURL
+
 actual class URL actual constructor(path: String) {
+    private val url = NSURL.URLWithString(path)!!
     actual fun toUri(): URI {
-        TODO("Not yet implemented")
+        return URI(url.host!!)
     }
 }
--- common/view/src/iosMain/kotlin/com/monoid/hackernews/common/view/itemlist/Item.ios.kt
+++ common/view/src/iosMain/kotlin/com/monoid/hackernews/common/view/itemlist/Item.ios.kt
@@ -1,6 +1,10 @@
 package com.monoid.hackernews.common.view.itemlist
 
+import platform.Foundation.NSURL
+
 actual class URL actual constructor(string: String) {
+    private val url = NSURL.URLWithString(string)!!
+
     actual val host: String
-        get() = TODO()
+        get() = url.host!!
 }
