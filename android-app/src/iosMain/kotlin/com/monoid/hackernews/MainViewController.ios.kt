2eaae200d2dc7be1f886157f10883c943a45b483
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2024-12-27 14:14:13 -0500

    Applied accessibility sync options to always for iOS

--- android-app/src/iosMain/kotlin/com/monoid/hackernews/MainViewController.ios.kt
+++ android-app/src/iosMain/kotlin/com/monoid/hackernews/MainViewController.ios.kt
@@ -1,5 +1,14 @@
+@file:OptIn(ExperimentalComposeApi::class)
+
 package com.monoid.hackernews
 
+import androidx.compose.runtime.ExperimentalComposeApi
+import androidx.compose.ui.platform.AccessibilitySyncOptions
 import androidx.compose.ui.window.ComposeUIViewController
 
-fun MainViewController() = ComposeUIViewController { App() }
+@Suppress("unused")
+fun MainViewController() = ComposeUIViewController(
+    configure = {
+        accessibilitySyncOptions = AccessibilitySyncOptions.Always(null)
+    },
+) { App() }
