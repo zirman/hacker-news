821070cd1214b40a3da4adc38761b5f0878f6f81
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2025-01-02 10:03:47 -0500

    Refactored desktop module to use convention plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,42 +1,35 @@
-//subprojects {
-//    tasks.withType<KotlinJvmCompile>().configureEach {
-//        compilerOptions {
-//            jvmTarget.set(JvmTarget.JVM_17)
-//        }
-//    }
-//    tasks.withType<KotlinCommonCompile>().configureEach {
-//        compilerOptions {
-            // keeps coroutine variables
-            // freeCompilerArgs.add("-Xdebug")
-            // recovers coroutine stack traces
-            // vm options ("-ea")
-            // https://github.com/Anamorphosee/stacktrace-decoroutinator
-
-//            extraWarnings.set(true)
-//            allWarningsAsErrors = false
+//tasks.withType<KotlinCommonCompile>().configureEach {
+//    compilerOptions {
+//        // keeps coroutine variables
+//        // freeCompilerArgs.add("-Xdebug")
+//        // recovers coroutine stack traces
+//        // vm options ("-ea")
+//        // https://github.com/Anamorphosee/stacktrace-decoroutinator
 //
+//        extraWarnings.set(true)
+//        allWarningsAsErrors = false
+//
+//        freeCompilerArgs.addAll(
+//            listOf(
+//                "-opt-in=kotlinx.coroutines.FlowPreview",
+//                "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
+//                "-P",
+//                "plugin:androidx.compose.compiler.plugins.kotlin:stabilityConfigurationPath=" +
+//                        rootDir.absolutePath + "/compose_compiler_config.conf",
+//            ),
+//        )
+//
+//        if (project.findProperty("hackernews.enableComposeCompilerReports") == "true") {
+//            // force tasks to rerun so that metrics are generated
+//            outputs.upToDateWhen { false }
 //            freeCompilerArgs.addAll(
 //                listOf(
-//                    "-opt-in=kotlinx.coroutines.FlowPreview",
-//                    "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
-//                    "-P",
-//                    "plugin:androidx.compose.compiler.plugins.kotlin:stabilityConfigurationPath=" +
-//                            rootDir.absolutePath + "/compose_compiler_config.conf",
+//                    "-P=plugin:androidx.compose.compiler.plugins.kotlin:reportsDestination=" +
+//                            projectDir.absolutePath + "/build/compose_metrics/",
+//                    "-P=plugin:androidx.compose.compiler.plugins.kotlin:metricsDestination=" +
+//                            projectDir.absolutePath + "/build/compose_metrics/",
 //                ),
 //            )
-//
-//            if (project.findProperty("hackernews.enableComposeCompilerReports") == "true") {
-//                // force tasks to rerun so that metrics are generated
-//                outputs.upToDateWhen { false }
-//                freeCompilerArgs.addAll(
-//                    listOf(
-//                        "-P=plugin:androidx.compose.compiler.plugins.kotlin:reportsDestination=" +
-//                                projectDir.absolutePath + "/build/compose_metrics/",
-//                        "-P=plugin:androidx.compose.compiler.plugins.kotlin:metricsDestination=" +
-//                                projectDir.absolutePath + "/build/compose_metrics/",
-//                    ),
-//                )
-//            }
 //        }
 //    }
 //}
