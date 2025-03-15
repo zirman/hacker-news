7bb71d5943522f0fff420eb6b5e5536f85d7acb3
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2025-03-15 17:20:11 -0400

    Updated Kotlin

--- mobile-app/src/iosMain/kotlin/com/monoid/hackernews/MainViewController.ios.kt
+++ mobile-app/src/iosMain/kotlin/com/monoid/hackernews/MainViewController.ios.kt
@@ -3,13 +3,12 @@
 package com.monoid.hackernews
 
 import androidx.compose.runtime.ExperimentalComposeApi
-import androidx.compose.ui.platform.AccessibilitySyncOptions
 import androidx.compose.ui.window.ComposeUIViewController
 import com.monoid.hackernews.common.view.App
 
 @Suppress("unused")
 fun MainViewController() = ComposeUIViewController(
     configure = {
-        accessibilitySyncOptions = AccessibilitySyncOptions.Always
+        // accessibilitySyncOptions = AccessibilitySyncOptions.Always
     },
 ) { App() }
