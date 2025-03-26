76f75bd3b7b7665c1091cb3374b5cecacb07642e
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2025-03-25 23:29:19 -0400

    UiModeManager.setApplicationNightMode() and AppCompatDelegate.setDefaultNightMode() set

--- mobile-app/src/androidMain/kotlin/com/monoid/hackernews/HNApplication.android.kt
+++ mobile-app/src/androidMain/kotlin/com/monoid/hackernews/HNApplication.android.kt
@@ -1,26 +1,50 @@
 package com.monoid.hackernews
 
 import android.app.Application
+import android.app.UiModeManager
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Build
 import android.os.StrictMode
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.getSystemService
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ProcessLifecycleOwner
+import androidx.lifecycle.lifecycleScope
+import com.monoid.hackernews.common.core.LoggerAdapter
+import com.monoid.hackernews.common.data.model.LightDarkMode
+import com.monoid.hackernews.common.data.model.SettingsRepository
 import com.monoid.hackernews.common.data.room.HNDatabase
 import io.ktor.client.HttpClient
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.distinctUntilChangedBy
+import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidFileProperties
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.GlobalContext.startKoin
+import org.koin.core.qualifier.named
 import org.koin.ksp.generated.module
 
 class HNApplication : Application() {
     private val remoteDataSource: HttpClient by inject()
     private val database: HNDatabase by inject()
+    private val settings: SettingsRepository by inject()
+    private val lifecycleOwner: LifecycleOwner by inject(named<ProcessLifecycleOwner>())
+    private val logger: LoggerAdapter by inject()
+
+    private val context = Dispatchers.Main.immediate + CoroutineExceptionHandler { coroutineContext, throwable ->
+        logger.recordException(
+            messageString = "CoroutineExceptionHandler",
+            throwable = throwable,
+            tag = TAG,
+        )
+    }
 
     override fun onCreate() {
         super.onCreate()
-
         StrictMode.setVmPolicy(
             StrictMode.VmPolicy.Builder()
                 .let {
@@ -32,17 +56,14 @@ class HNApplication : Application() {
                 }
                 .build(),
         )
-
         startKoin {
             androidContext(this@HNApplication)
             androidLogger()
             androidFileProperties()
             modules(ApplicationModule().module)
         }
-
         // TODO
         // updateAndPushDynamicShortcuts(MainActivity::class.java)
-
         // register locale changed broadcast receiver
         registerReceiver(
             /* receiver = */
@@ -50,6 +71,30 @@ class HNApplication : Application() {
             /* filter = */
             IntentFilter(Intent.ACTION_LOCALE_CHANGED),
         )
+        lifecycleOwner.lifecycleScope.launch(context) {
+            settings.preferences
+                .distinctUntilChangedBy { it.lightDarkMode }
+                .collect {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                        checkNotNull(getSystemService<UiModeManager>())
+                            .setApplicationNightMode(
+                                when (it.lightDarkMode) {
+                                    LightDarkMode.System -> UiModeManager.MODE_NIGHT_AUTO
+                                    LightDarkMode.Light -> UiModeManager.MODE_NIGHT_NO
+                                    LightDarkMode.Dark -> UiModeManager.MODE_NIGHT_YES
+                                }
+                            )
+                    } else {
+                        AppCompatDelegate.setDefaultNightMode(
+                            when (it.lightDarkMode) {
+                                LightDarkMode.System -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+                                LightDarkMode.Light -> AppCompatDelegate.MODE_NIGHT_NO
+                                LightDarkMode.Dark -> AppCompatDelegate.MODE_NIGHT_YES
+                            }
+                        )
+                    }
+                }
+        }
     }
 
     override fun onTerminate() {
@@ -58,3 +103,5 @@ class HNApplication : Application() {
         super.onTerminate()
     }
 }
+
+private const val TAG = "HNApplication"
