da6e9ee8789551cd666e7bb979b3658090e346a9
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2025-05-04 16:57:19 +0000

    Updated dependencies

--- common/view/src/commonMain/kotlin/com/monoid/hackernews/common/view/itemdetail/ToExtras.kt
+++ common/view/src/commonMain/kotlin/com/monoid/hackernews/common/view/itemdetail/ToExtras.kt
@@ -1,7 +1,6 @@
 package com.monoid.hackernews.common.view.itemdetail
 
 import androidx.compose.runtime.Composable
-import androidx.core.bundle.bundleOf
 import androidx.lifecycle.DEFAULT_ARGS_KEY
 import androidx.lifecycle.SAVED_STATE_REGISTRY_OWNER_KEY
 import androidx.lifecycle.VIEW_MODEL_STORE_OWNER_KEY
@@ -9,6 +8,7 @@ import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.MutableCreationExtras
 import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 import androidx.savedstate.SavedStateRegistryOwner
+import androidx.savedstate.savedState
 import com.monoid.hackernews.common.data.api.ItemId
 
 @Composable
@@ -16,9 +16,9 @@ internal fun ItemId.toExtras(): CreationExtras = MutableCreationExtras().apply {
     val viewModelStoreOwner = checkNotNull(LocalViewModelStoreOwner.current)
     set(
         DEFAULT_ARGS_KEY,
-        bundleOf(
-            ItemDetailViewModel.ITEM_ID to this@toExtras.long,
-        ),
+        savedState {
+            putLong(ItemDetailViewModel.ITEM_ID, this@toExtras.long)
+        },
     )
     set(VIEW_MODEL_STORE_OWNER_KEY, viewModelStoreOwner)
     set(SAVED_STATE_REGISTRY_OWNER_KEY, viewModelStoreOwner as SavedStateRegistryOwner)
