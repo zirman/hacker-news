a72cc21c109c9703dc8b9047f129e345982d5107
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2025-05-10 20:10:08 +0000

    Added top content padding to fab menu

--- common/view/src/androidMain/kotlin/com/monoid/hackernews/common/view/stories/StoriesFab.android.kt
+++ common/view/src/androidMain/kotlin/com/monoid/hackernews/common/view/stories/StoriesFab.android.kt
@@ -2,6 +2,10 @@
 
 package com.monoid.hackernews.common.view.stories
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.safeContent
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.FloatingActionButtonMenu
@@ -14,6 +18,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.semantics.CustomAccessibilityAction
 import androidx.compose.ui.semantics.customActions
 import androidx.compose.ui.semantics.isTraversalGroup
@@ -40,6 +45,14 @@ fun StoriesFab(
         },
         modifier = modifier,
     ) {
+        // top content padding
+        Spacer(
+            modifier = Modifier.height(
+                with(LocalDensity.current) {
+                    WindowInsets.safeContent.getTop(this).toDp()
+                },
+            ),
+        )
         for (i in FabAction.entries.indices) {
             val item = FabAction.entries[i]
             FloatingActionButtonMenuItem(
