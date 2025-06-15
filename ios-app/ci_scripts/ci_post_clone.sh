4e4b9c46c75b9c4cb9eff00662efdc579080779b
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2025-06-15 19:49:36 -0400

    Download Jetbrains Java in XCode Cloud

--- ios-app/ci_scripts/ci_post_clone.sh
+++ ios-app/ci_scripts/ci_post_clone.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 
-#curl -s "https://get.sdkman.io?ci=true" | bash
-#export SDKMAN_DIR="$HOME/.sdkman"
-#[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
-#sdk install java 21.0.7-jbr
+curl -s "https://get.sdkman.io?ci=true" | bash
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk install java 21.0.7-jbr
 #sdk env
 #export JAVA_HOME=/Users/local/.sdkman/candidates/java/current
-
-brew install openjdk@21
-
 #JAVA_HOME=/usr/local/opt/openjdk@21
-#export JAVA_HOME=/usr/local/opt/openjdk@21
--- ios-app/ci_scripts/ci_pre_xcodebuild.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-JAVA_HOME=/usr/local/opt/openjdk@21
-
-export JAVA_HOME=/usr/local/opt/openjdk@21
