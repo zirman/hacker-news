9ce745ed267f67fdb8e895f39dadd4e3d0502d50
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2025-06-15 16:24:48 -0400

    Download Jetbrains Java in XCode Cloud

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 curl -s "https://get.sdkman.io?ci=true" | bash
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk install 21.0.7-jbr
