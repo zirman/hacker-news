cd9ed4f0bb90b3023c125772d4a4b42214f50f5b
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2025-06-15 21:39:26 -0400

    Download Jetbrains Java in XCode Cloud

--- ios-app/ci_scripts/ci_post_clone.sh
+++ ios-app/ci_scripts/ci_post_clone.sh
@@ -3,6 +3,6 @@
 cd ../..
 curl -s "https://get.sdkman.io?ci=true" | bash
 SDKMAN_DIR="$HOME/.sdkman"
-"$SDKMAN_DIR/bin/sdkman-init.sh"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$SDKMAN_DIR/bin/sdkman-init.sh"
 sdk env install
 ./gradlew dependencies
