e329445aba0d8bc7f3418b84b1b86d851c6d9161
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2025-06-15 20:13:17 -0400

    Download Jetbrains Java in XCode Cloud

--- ios-app/ci_scripts/ci_post_clone.sh
+++ ios-app/ci_scripts/ci_post_clone.sh
@@ -5,4 +5,5 @@ export SDKMAN_DIR="$HOME/.sdkman"
 [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk install java 21.0.7-jbr
 #sdk env
-../gradlew dependencies
+pwd
+../../gradlew dependencies
