2bb3667dc58155e32bef4fdf99a16e61225d4353
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2026-05-10 19:07:05 -0400

    Implemented Settings persistence for the web target using DataStore and Kotlin Serialization.

--- common/data/src/webMain/kotlin/com/monoid/hackernews/common/data/model/Settings.web.kt
+++ common/data/src/webMain/kotlin/com/monoid/hackernews/common/data/model/Settings.web.kt
@@ -1,13 +1,32 @@
 package com.monoid.hackernews.common.data.model
 
+import androidx.datastore.core.CorruptionException
 import androidx.datastore.preferences.core.MutablePreferences
 import androidx.datastore.preferences.core.Preferences
+import io.ktor.utils.io.core.toByteArray
+import kotlinx.serialization.json.Json
 
-// TODO
 actual var MutablePreferences.settings: Settings?
-    get() = Settings()
-    set(value) {}
+    get() = try {
+        get(SETTINGS_KEY)
+            ?.decodeToString()
+            ?.let { Json.decodeFromString<Settings>(it) }
+    } catch (throwable: Throwable) {
+        throw CorruptionException("Cannot read preferences", throwable)
+    }
+    set(value) {
+        set(
+            SETTINGS_KEY,
+            Json.encodeToString(Settings.serializer(), checkNotNull(value))
+                .toByteArray(),
+        )
+    }
 
-// TODO
 actual val Preferences.settings: Settings?
-    get() = Settings()
+    get() = try {
+        get(SETTINGS_KEY)
+            ?.decodeToString()
+            ?.let { Json.decodeFromString<Settings>(it) }
+    } catch (throwable: Throwable) {
+        throw CorruptionException("Cannot read preferences", throwable)
+    }
